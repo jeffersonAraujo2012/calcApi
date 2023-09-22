5bab74f88fac7517df41b1b49fcaf628ccbf75a0
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-09-22 15:48:41 -0300

    Default values added to .env.example

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: function (origin, callback) {
+    origin: function (_, callback) {
       if (true) {
         callback(null, true)
       }
